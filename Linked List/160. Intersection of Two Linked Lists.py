2a4176410083985e763e3af21d8438e680ba949d
gp02gp03 <gp02gp03gmail.com>
2021-03-06 00:04:01 +0800

    add 160. Intersection of Two Linked Lists.py

---
+++ Linked List/160. Intersection of Two Linked Lists.py
@@ -0,0 +1,28 @@
+'''
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Reference of the node with value = 8
+Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: null
+Input Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+
+
+def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+    if not headA or not headB:
+        return None
+        pa, pb = headA, headB
+        while pa != pb:
+            if pa == None:
+                pa = headB
+            else:
+                pa = pa.next
+            if pb == None:
+                pb = headA
+            else:
+                pb = pb.next
+    return pa
+
+
+'''
